96ef133a69b0de42cff2adc02bf3730487af3072
Benjamin Anderson <46897951+BAnderson6895@users.noreply.github.com>
2019-04-10 12:14:04 -0400

    Revert "Pull"

--- 3d-printer-collecter/3d_printer_table.sql
+++ 3d-printer-collecter/3d_printer_table.sql
@@ -12,14 +12,8 @@ CREATE TABLE job_history(
   endtime VARCHAR(50),
   time_elapsed INT(50),
   status VARCHAR(15),
-  extruder1_temp FLOAT(50),
-  extruder2_temp FLOAT(50),
-  bedtemp FLOAT(50)
+  extruder1_temp INT(50),
+  extruder2_temp INT(50),
+  bedtemp INT(50)
 );
 
-
-INSERT INTO job_history(printer,filename,starttime,endtime,time_elapsed,status,extruder1_temp,extruder2_temp,bedtemp) VALUES
-('gutenberg', 'example1', '12-12-18-0' ,'12-13-18-0' , '5500','printing','19.3','23.3','22.2');
-
-INSERT INTO job_history(printer,filename,starttime,endtime,time_elapsed,status,extruder1_temp,extruder2_temp,bedtemp) VALUES
-('xerox', 'example2', '12-12-18-1000' ,'12-13-18-0' , '10000','printing','19.8','23.1','24.2');
--- 3d-printer-collecter/README.md
+++ 3d-printer-collecter/README.md
@@ -1,4 +1,4 @@
 # 3D_Printer_Project
-David, Justin, and Ben's 3D printer API project
+Rocco And David's 3D printer API project
 
 Must Source 3d_printer_table.sql before running
